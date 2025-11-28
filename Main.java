306dccae5b87eb3436cfec256f28348ed51bddb3
Ashwitha <ashwithachinni0602@gmail.com>
2025-11-28 23:36:52 +0530

    added

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    
+    // Your program begins with a call to main()
+    public static void main(String[] args)
+    {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+        System.out.println("Hii from vscode");
+    }
+}
